574518dbdcdfc4468a8116b45767bf0ba8a8cfba
climerl23 <66397864+climerl23@users.noreply.github.com>
2021-06-23 03:19:44 -0500

    Rosalind_Ex_1.py
    sliced string inclusively

---
+++ Roslind_Ex_1.py
@@ -0,0 +1,50 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 77,
+   "id": "1b394f07",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hadogenes vitticeps\n"
+     ]
+    }
+   ],
+   "source": [
+    "str = \"i6XRfnxdfnxkxHadogenesvk2xHbvRxK9iceKMPy3dLpxTWgARLDH9dDGo3z7KqRZz01ehZEI9IGwND1Q41KZP6zfbJxi1ZOeTAxGfmLuarSyY4aW8fLJQCJ7D2Os2S3JkcLIZkKr67M6LyBM5mEDgvRFpvitticepsKY3uq2fYV4S21D1Mt9nBt.\"\n",
+    "a =13\n",
+    "b =21\n",
+    "c =154\n",
+    "d =162\n",
+    "l=str[a:b+1]\n",
+    "s=str[c:d+1]\n",
+    "print(l, s)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
