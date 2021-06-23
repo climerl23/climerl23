63eada981d093a3c64933e100f8941c82fe89376
climerl23 <66397864+climerl23@users.noreply.github.com>
2021-06-23 04:48:15 -0500

    rosalind_Ex_3.py
    conditions and loops

---
+++ rosalind_Ex_3.py
@@ -0,0 +1,51 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 108,
+   "id": "1b394f07",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "16148881\n"
+     ]
+    }
+   ],
+   "source": [
+    "\n",
+    "a = 4440\n",
+    "b = 9182\n",
+    "\n",
+    "result = 0\n",
+    "for i in range(a,b+1):\n",
+    "  if i%2 == 1:\n",
+    "    result += i\n",
+    "print (result)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
